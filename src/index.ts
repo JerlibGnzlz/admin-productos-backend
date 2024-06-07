445e7fddad63ed39b5f40b92515b9421c5e4e459
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-06-07 20:28:42 -0300

    reorganizando archivos

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { server } from "./server";
-import colors from 'colors';
-
-const port = process.env.PORT || 3000
-
-server.listen(port, () => {
-    console.log(colors.bgGreen.bold(`Api en el puerto ${port}`))
-})
