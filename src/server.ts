27b46f0849d5d2c1e5d657768620a86f74b54ee1
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-06-07 19:23:55 -0300

    colors para los clg

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import express from "express"
 import router from "./routes"
 import { db } from "./config/db"
+import colors from "colors"
 
 
 async function connectDB() {
     try {
         await db.authenticate()
         db.sync()
-        console.log("Coneccion exitosa")
+        console.log(colors.bgBlue.bold("Coneccion exitosa"))
     } catch (error) {
         console.log(error)
-        console.log("Hubo un error al conectar la Base de Datos")
+        console.log(colors.bgRed.bold("Hubo un error al conectar la Base de Datos"))
     }
 }
 connectDB()
