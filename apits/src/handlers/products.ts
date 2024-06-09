2515441e95ff6e02e6afab0d95700680da2eab4b
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-06-09 01:21:17 -0300

    enviar a la DB

--- apits/src/handlers/products.ts
+++ apits/src/handlers/products.ts
@@ -1,5 +1,14 @@
 import { Request, Response } from "express"
+import Product from "../models/ProductModel"
 
-export const createProduct = (req: Request, res: Response) => {
-    res.json({ message: "desde post" })
+
+export const createProduct = async (req: Request, res: Response) => {
+
+    try {
+        const product = new Product(req.body)
+        await product.save()
+        res.status(201)
+    } catch (error) {
+        console.log(error)
+    }
 }
