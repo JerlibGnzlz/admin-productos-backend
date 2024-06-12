80e3fa4000fd6dad6a26e56105d32358cce4b2d2
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-06-11 23:57:59 -0300

    mi primer test

--- apits/src/__tests__/server.test.ts
+++ apits/src/__tests__/server.test.ts
@@ -2,4 +2,7 @@ describe("Nuestro promer test", () => {
     it("Debe revisar 1 + 1 = 2", () => {
         expect(1 + 1).toBe(2)
     })
+    it("Debe revisar 1 + 1 no sea 3", () => {
+        expect(1 + 1).not.toBe(3)
+    })
 })
