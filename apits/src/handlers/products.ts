2fe50482bb6af8522020d4b4668ee405fa0f4d5c
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-06-14 21:53:50 -0300

    metricas de validaciones >= 80 es lo recomendable y mejorando

--- apits/src/handlers/products.ts
+++ apits/src/handlers/products.ts
@@ -5,43 +5,31 @@ import { where } from "sequelize";
 
 export const createProduct = async (req: Request, res: Response) => {
 
-    try {
-        const product = await Product.create(req.body)
-        res.status(201).json({ data: product })
-    } catch (error) {
-        console.log(error)
-    }
+    const product = await Product.create(req.body)
+    res.status(201).json({ data: product })
 }
 
 
 export const allProduct = async (req: Request, res: Response) => {
-    try {
-        const products = await Product.findAll({
-            attributes: { exclude: ["createdAt", "updatedAt", "active"] }
-        })
+    const products = await Product.findAll({
+        attributes: { exclude: ["createdAt", "updatedAt", "active"] }
+    })
 
-        res.json({ data: products })
-    } catch (error) {
-        console.log(error)
-    }
+    res.json({ data: products })
 }
 
 
 export const getProductById = async (req: Request, res: Response) => {
 
     const { id } = req.params
-    try {
+    const product = await Product.findByPk(id)
 
-        const product = await Product.findByPk(id)
-
-        if (!product) {
-            return res.status(404).json({ error: "Producto no encontrado" })
-        } else {
-            return res.json({ data: product })
-        }
-    } catch (error) {
-        console.log(error)
+    if (!product) {
+        return res.status(404).json({ error: "Producto no encontrado" })
+    } else {
+        return res.json({ data: product })
     }
+
 }
 
 export const updateProduct = async (req: Request, res: Response) => {
@@ -49,17 +37,13 @@ export const updateProduct = async (req: Request, res: Response) => {
     const { id } = req.params
     const product = await Product.findByPk(id)
 
-    try {
-        if (!product) {
-            return res.status(404).json({ error: "Producto no encontrado" })
-        } else {
-            await product.update(req.body)
-            return res.json({ data: product })
-        }
-
-    } catch (error) {
-        console.log(error)
+    if (!product) {
+        return res.status(404).json({ error: "Producto no encontrado" })
+    } else {
+        await product.update(req.body)
+        return res.json({ data: product })
     }
+
 }
 
 export const updateActive = async (req: Request, res: Response) => {
