fe66df2adbb14e6e013a5fbe167ab6071d4dd8ec
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-06-28 17:58:44 -0300

    components nuevo producto

--- client/src/layout/Layout.tsx
+++ client/src/layout/Layout.tsx
@@ -5,8 +5,17 @@ import { Outlet } from "react-router-dom";
 export const Layout = () => {
     return (
         <>
-            Desde Layouts
-            <Outlet />
+            <header className="bg-slate-800" >
+                <div className="mx-auto max-w-6xl py-10">
+                    <h1 className="text-4xl text-white font-extrabold  ">
+                        Administrador de Productos
+                    </h1>
+                </div>
+            </header>
+
+            <main className="mx-auto max-w-6xl mt-10 p-10 bg-white shadow-xl">
+                <Outlet />
+            </main>
         </>
     )
 };
